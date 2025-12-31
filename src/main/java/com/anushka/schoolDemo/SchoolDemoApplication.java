1333dfb932952f33105ae05e4e3a8b612b79c7c0
AnushkaSamaranayake <anushkasandeepa522@gmail.com>
2025-12-31 14:28:06 +0530

    Jwt authentication filtering added

--- src/main/java/com/anushka/schoolDemo/SchoolDemoApplication.java
+++ src/main/java/com/anushka/schoolDemo/SchoolDemoApplication.java
@@ -4,11 +4,14 @@ import com.anushka.schoolDemo.repository.StudentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SchoolDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolDemoApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("teacher123"));
+
 	}
 }
--- src/main/java/com/anushka/schoolDemo/security/config/SecurityConfig.java
+++ src/main/java/com/anushka/schoolDemo/security/config/SecurityConfig.java
@@ -2,15 +2,63 @@ package com.anushka.schoolDemo.security.config;
 
 // Password encoding, authentication managers, security filters, CORS configuration, etc.
 
+import com.anushka.schoolDemo.security.jwt.JwtAuthenticationFilter;
+import com.anushka.schoolDemo.security.user.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(
+            CustomUserDetailsService userDetailsService,
+            JwtAuthenticationFilter jwtAuthenticationFilter
+    ){
+        this.userDetailsService = userDetailsService;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // Public endpoints
+                        .requestMatchers("/auth/**").permitAll()
+                        // Protected endpoints
+                        // ADMIN only
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+
+                        //ADMIN access Students
+                        .requestMatchers("/api/admin/students/**").hasRole("ADMIN")
+
+                        // TEACHER only
+                        .requestMatchers("/api/teachers/**").hasRole("TEACHER")
+
+                        // STUDENT only
+                        .requestMatchers("/api/students/**").hasRole("STUDENT")
+
+                        // Any authenticated user
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -23,4 +71,6 @@ public class SecurityConfig {
     ) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
+
 }
